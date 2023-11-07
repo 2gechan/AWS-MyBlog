7129ed104faf25ed6273c13982537f70705e35f7
2gechan <7532602@gmail.com>
2023-11-08 01:47:46 +0900

    배포 테스트

--- src/test/java/com/gechan/myblog/MyblogApplicationTests.java
+++ src/test/java/com/gechan/myblog/MyblogApplicationTests.java
@@ -3,10 +3,10 @@ package com.gechan.myblog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class MyblogApplicationTests {
 
-    @Test
+
     void contextLoads() {
     }
 
